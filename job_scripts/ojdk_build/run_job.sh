ce3331d8656f54833d263713196d52229931a00e
dsamersoff <dms@samersoff.net>
2020-09-04 09:15:36 +0300

    shell based jobs

--- job_scripts/ojdk_build/run_job.sh
+++ job_scripts/ojdk_build/run_job.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
-source $JENKINS_HOME/scripts/`uname -n`_config.sh
+source $JENKINS_HOME/job_scripts/common/config.sh
+
+if [ "x$1" = "x" ]
+then
+  echo "Workspace to build shall be specified e.g. jdk14"
+  exit 7
+fi
 
 cd ${JDK_WORKSPACE_ROOT}
-/bin/sh -xe $JENKINS_HOME/scripts/specjbb_scripts/buildscripts/jdk_update.sh ${JDK_WORKSPACE}
+/bin/sh -xe $JENKINS_HOME/job_scripts/common/buildscripts/jdk_update.sh $1
 
-cd ${JDK_WORKSPACE_ROOT}/${JDK_WORKSPACE}
-/bin/sh -xe $JENKINS_HOME/scripts/specjbb_scripts/buildscripts/jdk_build.sh --product 
+cd ${JDK_WORKSPACE_ROOT}/$1
+/bin/sh -xe $JENKINS_HOME/job_scripts/common/buildscripts/jdk_build.sh --product 
 make CONF=release images
--- job_scripts/specjbb_run/run_job.sh
+++ job_scripts/specjbb_run/run_job.sh
@@ -1,19 +1,30 @@
 #!/bin/sh
 
-source $JENKINS_HOME/scripts/`uname -n`_config.sh
+source $JENKINS_HOME/job_scripts/common/config.sh
 
-cd $JENKINS_HOME/scripts/specjbb_scripts
+if [ "x$1" = "x" ]
+then
+  echo "Workspace to build shall be specified e.g. jdk14"
+  exit 7
+fi
+
+_jdk_workspace=$1
+
+# Setup environment on the remote machine
+cd $JENKINS_HOME/job_scripts/common
 
 tar czf - runscripts | ssh ${JBB_REMOTE} "mkdir -p ${JBB_RUN_ROOT}/${JBB_RUN_NAME}; cd ${JBB_RUN_ROOT}/${JBB_RUN_NAME} && tar xzvf -"
-scp $JENKINS_HOME/scripts/specjbb_scripts/bin/specjbb.py $JBB_REMOTE:${JBB_RUN_ROOT}/${JBB_RUN_NAME}
+scp $JENKINS_HOME/job_scripts/common/specjbb.py $JBB_REMOTE:${JBB_RUN_ROOT}/${JBB_RUN_NAME}
 
-_jdk=${JDK_WORKSPACE_ROOT}/${JDK_WORKSPACE}
-_jdk_name=${JDK_WORKSPACE}
+# Guess jdk image and copy it to remote machine
 
-if [ -f ${JDK_WORKSPACE_ROOT}/${JDK_WORKSPACE}/build/linux-aarch64-server-release/images/jdk/bin/java ]
+if [ -f ${JDK_WORKSPACE_ROOT}/${_jdk_workspace}/build/linux-aarch64-server-release/images/jdk/bin/java ]
 then
-   _jdk=${JDK_WORKSPACE_ROOT}/${JDK_WORKSPACE}/build/linux-aarch64-server-release/images/jdk
+   _jdk=${JDK_WORKSPACE_ROOT}/${_jdk_workspace}/build/linux-aarch64-server-release/images/jdk
    _jdk_name="jdk"
+else
+  _jdk=${JDK_WORKSPACE_ROOT}/${_jdk_workspace}
+  _jdk_name=${JDK_WORKSPACE}
 fi 
   
 if [ ! -f ${_jdk}/bin/java ]
@@ -25,13 +36,19 @@ fi
 cd ${_jdk}/..
 tar czf - ${_jdk_name} | ssh ${JBB_REMOTE} "cd ${JBB_RUN_ROOT}/${JBB_RUN_NAME} && tar xzvf -"
 
+# Create a run script on the remote side, that sets some environment variables
 cat  | ssh ${JBB_REMOTE} "cd ${JBB_RUN_ROOT}/${JBB_RUN_NAME} && tee runme_tmp.sh" << EOM 
 export JAVA_HOME=${JBB_RUN_ROOT}/${JBB_RUN_NAME}/${_jdk_name}
 export JBB_HOME=${JBB_HOME} 
+export JOB_NAME=${JOB_NAME}
 cd runscripts 
-/bin/sh ./runme.sh 1  
+echo ${JOB_NAME} > README.md
+/bin/sh ./runme.sh
 EOM
 
+# Run specjbb
 ssh -n $JBB_REMOTE "cd ${JBB_RUN_ROOT}/${JBB_RUN_NAME} && sudo /bin/sh ./runme_tmp.sh"
-ssh -n $JBB_REMOTE "cd ${JBB_RUN_ROOT}/${JBB_RUN_NAME} && /usr/bin/env python3 ./specjbb.py -o ${JBB_RUN_NAME}.xlsx"
+
+# Gather results and copy it back to jenkins machine
+ssh -n $JBB_REMOTE "cd ${JBB_RUN_ROOT}/${JBB_RUN_NAME} && ./specjbb.py -o ${JBB_RUN_NAME}.xlsx"
 scp -r ${JBB_REMOTE}:${JBB_RUN_ROOT}/${JBB_RUN_NAME}/${JBB_RUN_NAME}.xlsx .
