d2c631f2360eaa48591ffcb4d376f4cbd5d5e59d
dsamersoff <dms@samersoff.net>
2020-09-04 11:23:07 +0300

    Bugfix

--- job_scripts/specjbb_run/run_job.sh
+++ job_scripts/specjbb_run/run_job.sh
@@ -20,9 +20,8 @@ _jdk_workspace=$1
 # Setup environment on the remote machine
 cd $JENKINS_HOME/specjbb_scripts
 
-tar czf - runscripts | ssh ${JBB_REMOTE} "mkdir -p ${JBB_RUN_ROOT}/${JBB_RUN_NAME}; cd ${JBB_RUN_ROOT}/${JBB_RUN_NAME} && tar xzvf -"
-chmod a+x $JENKINS_HOME/specjbb_scripts/bin/specjbb.py 
-scp $JENKINS_HOME/specjbb_scripts/bin/specjbb.py $JBB_REMOTE:${JBB_RUN_ROOT}/${JBB_RUN_NAME}
+tar czf - runscripts | ssh ${JBB_REMOTE} "mkdir -p ${JBB_RUN_ROOT}/${JOB_NAME}; cd ${JBB_RUN_ROOT}/${JOB_NAME} && tar xzvf -"
+scp $JENKINS_HOME/specjbb_scripts/bin/specjbb.py $JBB_REMOTE:${JBB_RUN_ROOT}/${JOB_NAME}
 
 # Guess jdk image and copy it to remote machine
 if [ -f ${JDK_WORKSPACE_ROOT}/${_jdk_workspace}/build/linux-aarch64-server-release/images/jdk/bin/java ]
@@ -40,22 +39,24 @@ then
   exit 255
 fi  
 
+echo "Copying ${_jdk} to ${JBB_REMOTE}:${JBB_RUN_ROOT}/${JOB_NAME}/${_jdk_name}"
+
 cd ${_jdk}/..
-tar czf - ${_jdk_name} | ssh ${JBB_REMOTE} "cd ${JBB_RUN_ROOT}/${JBB_RUN_NAME} && tar xzvf -"
+tar czf - ${_jdk_name} | ssh ${JBB_REMOTE} "cd ${JBB_RUN_ROOT}/${JOB_NAME} && tar xzvf -"
 
 # Create a run script on the remote side, that sets some environment variables
-cat  | ssh ${JBB_REMOTE} "cd ${JBB_RUN_ROOT}/${JBB_RUN_NAME} && tee runme_tmp.sh" << EOM 
-export JAVA_HOME=${JBB_RUN_ROOT}/${JBB_RUN_NAME}/${_jdk_name}
+cat  | ssh ${JBB_REMOTE} "cd ${JBB_RUN_ROOT}/${JOB_NAME} && tee runme_tmp.sh" << EOM 
+export JAVA_HOME=${JBB_RUN_ROOT}/${JOB_NAME}/${_jdk_name}
 export JBB_HOME=${JBB_HOME} 
 export JOB_NAME=${JOB_NAME}
 cd runscripts 
 echo ${JOB_NAME} > README.md
 /bin/sh ./runme.sh
+python3 ./specjbb.py -o ${JOB_NAME}.xlsx
 EOM
 
 # Run specjbb
-ssh -n $JBB_REMOTE "cd ${JBB_RUN_ROOT}/${JBB_RUN_NAME} && sudo /bin/sh ./runme_tmp.sh"
+ssh -n $JBB_REMOTE "cd ${JBB_RUN_ROOT}/${JOB_NAME} && sudo /bin/sh ./runme_tmp.sh"
 
-# Gather results and copy it back to jenkins machine
-ssh -n $JBB_REMOTE "cd ${JBB_RUN_ROOT}/${JBB_RUN_NAME} && ./specjbb.py -o ${JBB_RUN_NAME}.xlsx"
-scp -r ${JBB_REMOTE}:${JBB_RUN_ROOT}/${JBB_RUN_NAME}/${JBB_RUN_NAME}.xlsx .
+# Copy results back to jenkins machine
+scp -r ${JBB_REMOTE}:${JBB_RUN_ROOT}/${JOB_NAME}/${JOB_NAME}.xlsx .
