0e8a3ec066345baf1e8c4f697734c61c6a8e9cda
dsamersoff <dms@samersoff.net>
2023-01-08 16:31:38 +0300

    Improve searching of testjava

--- bin/testjava.py
+++ bin/testjava.py
@@ -11,13 +11,13 @@ dirlist = []
 
 while True:
   """ Check for image first """
-  d = glob.glob(thispath + "/*/build/linux-*/images/jdk")
+  d = glob.glob(thispath + "/*/build/*/images/jdk")
   if len(d) > 0:
     dirlist += d
     break
 
   """ Try to find exploded image """
-  d = glob.glob(thispath + "/*/build/linux-*/jdk")
+  d = glob.glob(thispath + "/*/build/*/jdk")
   if len(d) > 0:
     dirlist += d
     break
@@ -31,19 +31,23 @@ while True:
   if len(thispath) == 3 and thispath[1] == ":":
     break
 
-
-if len(dirlist) == 0:
-  if len(sys.argv) > 1 and sys.argv[1] == "-all":
-    print ("Nothing found")
-  sys.exit(-1)
-
 tpl = []
 for d in dirlist:
   tpl.append( (d, os.path.commonpath([cwd,d])) )
 tpl = sorted(tpl, key=lambda n: len(n[1]), reverse=True)
 
-if len(sys.argv) > 1 and sys.argv[1] == "-all":
+if len(sys.argv) <= 1 or sys.argv[1] != "--all":
+  """Attempt to guess the best option"""
+  un = os.uname()
+  cands=list()
   for d in tpl:
+    if d[1] != cwd:
+      continue
+    if d[0].find(un.machine) == -1:
+      continue
+    cands.append(d)
+  tpl = cands
+
+"""Print all survived candidates"""
+for d in tpl:
     print(d[0])
-else:
-  print(tpl[0][0])
