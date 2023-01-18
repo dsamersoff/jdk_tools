945b1c3dec74cf9d98a815bfb773308a23c5efcf
dsamersoff <dms@samersoff.net>
2023-01-18 18:30:35 +0300

    On-flight diagnostic

--- smoketest/c2syntest/syntest/SynTest.java
+++ smoketest/c2syntest/syntest/SynTest.java
@@ -9,19 +9,32 @@ import org.objectweb.asm.util.*;
 
 import java.lang.Math;
 import java.io.PrintWriter;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryPoolMXBean;
+import java.lang.management.MemoryUsage;
+import java.lang.management.MemoryType;
+
+import javax.management.ObjectName;
+import javax.management.MBeanServer;
+
 
 public class SynTest implements Opcodes {
   private final static String packageName = "syntest";
   private final static String classNamePrefix = "SynImpl";
-  private final static int numClasses = 40_000;
+  private final static int numClasses = 20_000;
   private final static SynTestRunner dtrs[] = new SynTestRunner[numClasses];
   private final static int warmUps = 10_000;
-  private final static int numBatches = 10_000;
-  private final static long timesPerBatch = 1000;
+  private final static int numBatches = 1_000;
+  private final static long timesPerBatch = 1_000;
   private final static long batchTime[] = new long[numBatches];
   private final static boolean traceMethodGeneration = false;
 
   private static long runResult = 0;
+
+  private static List<MemoryPoolMXBean> memoryBeans;
 
   static class SynClassLoader extends ClassLoader {
     public Class<?> defineClass(String name, byte[] b) {
@@ -135,6 +148,63 @@ public class SynTest implements Opcodes {
     return cw.toByteArray();
   }
 
+  public static void printMemoryUsage() {
+    System.err.println("---------- Memory Usage ----------");
+    for (MemoryPoolMXBean m: memoryBeans) {
+      MemoryUsage u = m.getUsage();
+      double used = u.getUsed();
+      // Skip Java Heap
+      if (m.getType() == MemoryType.NON_HEAP) {
+         System.err.println(m.getName() + " " + used);
+      }
+    }
+    System.err.println("---------------------------------");
+  }
+
+  public static void printCompiledMethods() {
+    try {
+      ObjectName objectName = new ObjectName("com.sun.management:type=DiagnosticCommand");
+      MBeanServer mbeanServer = ManagementFactory.getPlatformMBeanServer();
+
+      String operationName = "compilerCodelist"; //vmFlags
+      Object[] params = new Object[1];
+      String[] signature = new String[]{String[].class.getName()};
+
+      String result = (String) mbeanServer.invoke(objectName, operationName, params, signature);
+
+      List<String> filtered = result.lines().filter(
+        (line) -> line.contains(packageName + ".SynImpl_")
+      ).collect(Collectors.toList());
+
+      long all_count = filtered.size();
+
+      long c2_count = filtered.stream().filter(
+        (line) -> {
+          String fields[] = line.split(" ");
+          return Integer.valueOf(fields[1]) == 4;
+        }
+      ).count();
+
+      /*
+      result.lines().filter(
+        (line) -> line.contains(packageName + ".SynImpl_")
+      ).forEach(System.out::println);
+      */
+
+      System.out.printf("Found compiled syntetic methods, all: %d c2: %d\n", all_count, c2_count);
+    } catch(Throwable ex) {
+       ex.printStackTrace();
+    }
+  }
+
+  public static void printStat(int numItems) {
+    do_print_stat(numItems, false);
+  }
+
+  public static void printFinalStat(int numItems) {
+    do_print_stat(numItems, true);
+  }
+
   public static long do_measure(long times) {
     long start = System.currentTimeMillis();
     for (long j = 0; j < times; ++j) {
@@ -146,36 +216,44 @@ public class SynTest implements Opcodes {
     return end - start;
   }
 
-  public static void print_stat(int numItems) {
-      double total = 0;
-      for (int i = 0; i < numItems; ++i) {
-        total += batchTime[i];
-      }
+  public static void do_print_stat(int numItems, boolean last_call) {
+    long total = 0;
+    for (int i = 0; i < numItems; ++i) {
+      total += batchTime[i];
+    }
 
-      double mean = total / numItems;
+    double mean = total / numItems;
 
-      double variance = 0;
-      for (int i = 0; i < numItems; i++) {
-        variance += Math.pow(batchTime[i] - mean, 2);
-      }
-      variance = variance / numItems;
+    double variance = 0;
+    for (int i = 0; i < numItems; i++) {
+      variance += Math.pow(batchTime[i] - mean, 2);
+    }
+    variance = variance / numItems;
 
-      double std = Math.sqrt(variance);
+    double std = Math.sqrt(variance);
 
-      System.out.printf("Executed %d classes, %d times in %d batches\n", numClasses, (long)(numItems * timesPerBatch), numItems);
-      System.out.printf("Results %f (%f +- %f)\n", total, mean, std);
-  }
+    System.out.printf("Executed %d classes, %d times in %d batches\n", numClasses, (long)(numItems * timesPerBatch), numItems);
+    if (last_call) {
+      System.out.print("Final ");
+    }
+    System.out.printf("Results %d (%f +- %f)\n", total, mean, std);
+}
 
   public static void main(String args[]) {
     System.out.println("Syntetic test started");
+
     try {
+      memoryBeans = ManagementFactory.getMemoryPoolMXBeans();
+      printMemoryUsage();
+      printCompiledMethods();
+
       System.out.println("Generating");
       for (int i = 0; i < numClasses; ++i) {
         String className = String.format("%s_%04d", classNamePrefix, i);
         byte[] class_ba = doGenerateClass(className);
         SynClassLoader loader = new SynClassLoader();
         Class<?> clazz = loader.defineClass(packageName + "." + className, class_ba);
-        dtrs[i] = (SynTestRunner) clazz.newInstance();
+        dtrs[i] = (SynTestRunner) clazz.getDeclaredConstructor().newInstance();
       }
 
       System.out.println("Smoke check: " + dtrs[0].add(2,2) + " " + dtrs[0].doit(2,2,2));
@@ -188,15 +266,22 @@ public class SynTest implements Opcodes {
       }
       System.out.println("Warmup result:" + runResult);
 
+      printCompiledMethods();
+      printMemoryUsage();
+
       System.out.println("Executing");
       for (int i = 0; i < numBatches; ++i) {
         batchTime[i] = do_measure(timesPerBatch);
-        print_stat(i+1);
+        printStat(i+1);
       }
 
-      System.out.println("Exec result:" + runResult);
+      System.out.println("Execution result:" + runResult);
 
-      print_stat(numBatches);
+      printCompiledMethods();
+      printMemoryUsage();
+
+      printFinalStat(numBatches);
+
     } catch(Throwable ex) {
        ex.printStackTrace();
     }
