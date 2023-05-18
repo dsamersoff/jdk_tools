fec89533625318daf57e5a2100905c57eb29aa21
dsamersoff <dms@samersoff.net>
2023-05-18 11:05:28 +0300

    Better support for crosstargets in testjava.py

--- bin/testjava.py
+++ bin/testjava.py
@@ -10,25 +10,21 @@ cwd = os.getcwd()
 dirlist = []
 
 while True:
-  """ Check for image first """
-  d = glob.glob(thispath + "/*/build/*/images/jdk")
+  d = glob.glob(thispath + "/*/build/*/images/jdk")  # Check for image first
   if len(d) > 0:
     dirlist += d
     break
 
-  """ Try to find exploded image """
-  d = glob.glob(thispath + "/*/build/*/jdk")
+  d = glob.glob(thispath + "/*/build/*/jdk")  #  Try to find exploded image 
   if len(d) > 0:
     dirlist += d
     break
 
-  """ Nothing found, do one step upward """
-  (thispath, tail) = os.path.split(thispath)
+  (thispath, tail) = os.path.split(thispath)  # Nothing found, do one step upward 
   if thispath == '/':
     break
 
-  """ Handle windows drive letter D:/ """
-  if len(thispath) == 3 and thispath[1] == ":":
+  if len(thispath) == 3 and thispath[1] == ":":  #  Handle windows drive letter D:/ 
     break
 
 tpl = []
@@ -36,8 +32,10 @@ for d in dirlist:
   tpl.append( (d, os.path.commonpath([cwd,d])) )
 tpl = sorted(tpl, key=lambda n: len(n[1]), reverse=True)
 
-if len(sys.argv) <= 1 or sys.argv[1] != "--all":
-  """Attempt to guess the best option"""
+
+print_all = (len(sys.argv) > 1) and (sys.argv[1] in [ "--all", "-a" ])
+
+if not print_all:  # Filter list of candidates in attempt to guess the best option
   un = os.uname()
   cands=list()
   for d in tpl:
@@ -46,8 +44,8 @@ if len(sys.argv) <= 1 or sys.argv[1] != "--all":
     if d[0].find(un.machine) == -1:
       continue
     cands.append(d)
-  tpl = cands
+  if len(cands) > 0:  # No good candidates left, print all found
+    tpl = cands
 
-"""Print all survived candidates"""
-for d in tpl:
+for d in tpl:  # Print all found or survived candidates
     print(d[0])
