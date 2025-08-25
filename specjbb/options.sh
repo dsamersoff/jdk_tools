e53681c67ac0900e5aa6e465596dd73511f55286
dsamersoff <dms@samersoff.net>
2025-08-25 12:26:31 +0000

    Refactoring of specjbb scripts

--- specjbb/options.sh
+++ specjbb/options.sh
@@ -1,154 +1,181 @@
 #!/bin/bash
-OPT_VERSION="2.03 2020-10-30"
+OPT_VERSION="3.01 2025-08-23"
 
-# JAVA_HOME and JBB_HOME 
-# that comes from the environment overrrides default values
-
-if [ "x$JAVA_HOME" = "x" ]
-then
-JAVA_HOME="/opt/jdk"
-fi
-
-if [ "x$JBB_HOME" = "x" ]
-then
+# JAVA_HOME and JBB_HOME
+JAVA_HOME="/opt/jdk-11"
 JBB_HOME="/opt/specjbb2015-1.03a"
-fi
 
-# Use tail -f --pid=${pid} to display controller log
-# instead of shell wait. 
-# Handy, especially under jenkins, but may cause the script to stuck
-TAIL_WAIT=Yes
-
-# Check active usesr, docker presence and other conditions that may affect results
+# Check active users, docker presence and other conditions that may affect results
 # To run under jenkins you may need to disable this check
 VALIDATE_ENV=Yes
 
+# Use or not numactl
+# 1 or 2 sockets supported
+# GROUPS 0 NUMA Yes means composite run bound to 1 socket
+NUMA=Yes
+
+# Set OS parameters using sysctl
+SET_SYSTEM=Yes
+
+# Multivm setup, ignored in composite mode
+
 # Number of Groups (TxInjectors mapped to Backend) to expect
 # Support 0, 1, 2, 4 groups. 0 means composite
+# Note: 4 groups on 1 socket and 3 groups doesn't have sense
 GROUP_COUNT=2
 
 # Number of TxInjector JVMs to expect in each Group
 TI_JVM_COUNT=1
 
-# Memory usage
-# Memory is not is not adjusted to group count and passed as is. Take care.
-# Young gen pages should be less than total pages
-PAGE_SIZE=512
-PAGES=8
-YOUNG_PAGES=2
-
-# Use or not numactl 
-# 1 or 2 sockets supported
-# GROUPS 0 NUMA Yes means composite run bound to 1 socket
-NUMA=Yes
-
-# Benchmark options for Controller / TxInjector JVM / Backend
-# Please use -Dproperty=value to override the default and property file value
-# Please add -Dspecjbb.controller.host=$CTRL_IP (this host IP) to the benchmark options for the all components
-# and -Dspecjbb.time.server=true to the benchmark options for Controller 
-# when launching MultiJVM mode in virtual environment with Time Server located on the native host.
-if [ $GROUP_COUNT -ge 1 ]
-then
-SPEC_OPTS_C="\
-        -Dspecjbb.group.count=$GROUP_COUNT \
-        -Dspecjbb.txi.pergroup.count=$TI_JVM_COUNT \
+# Tuning
+# For controller in MVM mode
+SPEC_OPTS="\
+    -Dspecjbb.comm.connect.timeouts.connect=700000 \
+    -Dspecjbb.comm.connect.timeouts.read=700000 \
+    -Dspecjbb.comm.connect.timeouts.write=700000 \
+    -Dspecjbb.customerDriver.threads.probe=80 \
+    -Dspecjbb.forkjoin.workers.Tier1=80 \
+    -Dspecjbb.forkjoin.workers.Tier2=1 \
+    -Dspecjbb.forkjoin.workers.Tier3=16 \
+    -Dspecjbb.heartbeat.period=100000 \
+    -Dspecjbb.heartbeat.threshold=1000000 \
 "
-fi
 
-SPEC_OPTS_C="$SPEC_OPTS_C \
-        -Dspecjbb.controller.rtcurve.warmup.step=0.7 \
-        -Dspecjbb.forkjoin.workers.Tier1=100 \
-        -Dspecjbb.forkjoin.workers.Tier2=1 \
-        -Dspecjbb.forkjoin.workers.Tier3=16 \
-        -Dspecjbb.customerDriver.threads=64 \
-	-Dsecjbb.heartbeat.threshold=900000
+# For Backend in MVM mode
+JAVA_OPTS="\
+    -server \
+    -Xms124g \
+    -Xmx124g \
+    -Xmn114g \
+    -XX:SurvivorRatio=20 \
+    -XX:MaxTenuringThreshold=15 \
+    -XX:+UseLargePages \
+    -XX:LargePageSizeInBytes=2m \
+    -XX:+UseParallelGC \
+    -XX:+AlwaysPreTouch \
+    -XX:-UseAdaptiveSizePolicy \
+    -XX:-UsePerfData \
+    -XX:ParallelGCThreads=40 \
+    -XX:+UseTransparentHugePages \
+    -XX:+UseCompressedOops \
+    -XX:ObjectAlignmentInBytes=32 \
+"
+
+# JDK8  DEBUG_OPTS="-XX:+PrintGC -XX:+PrintGCDetails -Xloggc"
+DEBUG_OPTS="-Xlog:gc=debug,heap*=debug,phases*=debug,gc+age=debug"
+
+SYSCTL_OPTS="\
+    net.ipv4.conf.lo.accept_local=1 \
+    vm.swappiness=0 \
+    kernel.shmmax=16777216 \
+"
+
+SYSFILE_OPTS="\
+    /sys/kernel/mm/transparent_hugepage/enabled=always \
+    /sys/kernel/mm/transparent_hugepage/defrag=always \
+    /proc/sys/kernel/numa_balancing=0 \
+"
+
+
+#--------- MVM mode only settings
+
+# Benchmark options for Controller JVM
+SPEC_OPTS_CTL="\
+    ${SPEC_OPTS} \
+    -Dspecjbb.group.count=$GROUP_COUNT \
+    -Dspecjbb.txi.pergroup.count=$TI_JVM_COUNT \
 "
 
 SPEC_OPTS_TI="\
 "
-        
+
 SPEC_OPTS_BE="\
 "
 
-# Java options for Controller / TxInjector / Backend JVM
-JAVA_OPTS_COMMON="\
-        -server \
-        -XX:+PrintFlagsFinal \
+# Controller java options
+JAVA_OPTS_CTL="\
         -XX:+UseParallelGC \
-"	
-JAVA_OPTS_TUNING="\
-        -Xnoclassgc \
-        -XX:-UseAdaptiveSizePolicy \
-        -XX:+AlwaysPreTouch \
-        -XX:+UseBiasedLocking \
-        -XX:-UsePerfData \
-        -XX:-UseNUMA \
-        -XX:-UseNUMAInterleaving \
-        -XX:InlineSmallCode=20k \
-        -XX:CompileThreshold=1000 \
-        -XX:+AvoidUnalignedAccesses \
-        -XX:+UseSIMDForMemoryOps \
-        -XX:-UseFPUForSpilling \
-        -XX:-SegmentedCodeCache \
-        -XX:MaxInlineLevel=15 \
+        -XX:ParallelGCThreads=3 \
+        -Xms2g  \
+        -Xmx2g  \
+        -Xmn1536m \
 "
 
-# Not used in composite mode
-JAVA_OPTS_C="${JAVA_OPTS_COMMON} \
-             -XX:ParallelGCThreads=3 \
-             -Xms2g  \
-             -Xmx2g  \
-             -Xmn1536m \
+# Injector java options
+JAVA_OPTS_TI="\
+        -XX:+UseParallelGC \
+        -XX:ParallelGCThreads=3 \
+        -Xms2g  \
+        -Xmx2g  \
+        -Xmn1536m \
 "
 
-JAVA_OPTS_TI="${JAVA_OPTS_COMMON} \
-             -XX:ParallelGCThreads=3 \
-             -Xms2g  \
-             -Xmx2g  \
-             -Xmn1536m \
+# Backend java options
+JAVA_OPTS_BE="\
+        ${JAVA_OPTS} \
 "
 
-pages=$PAGES
-page_sz=$PAGE_SIZE
-mem_max=$(($pages * $page_sz))
-mem_young=$(($YOUNG_PAGES * $page_sz))
-mem_max+="m"
-mem_young+="m"
-
-JAVA_OPTS_BE="${JAVA_OPTS_COMMON} \
-              ${JAVA_OPTS_TUNING} \
-             -XX:ParallelGCThreads=56 \
-             -Xmx$mem_max \
-             -Xms$mem_max  \
-             -Xmn$mem_young \
-             -XX:SurvivorRatio=130 \
-             -XX:TargetSurvivorRatio=66 \
-             -XX:MaxTenuringThreshold=15 \
-"
-
-
-# Optional arguments for multiController / TxInjector / Backend mode 
-# For more info please use: java -jar specjbb2015.jar -m <mode> -h
-MODE_ARGS_C=""
-MODE_ARGS_TI=""
-MODE_ARGS_BE=""
-
-# ----- Reporting ----------------
+# ----- Validation and options pre-processing ----------------
 # Nothing to edit below this line
 # ---------------------------------
 
+if [ "x$NUMA" = "xYes" ]
+then
+    NUMA_CMD="/usr/bin/numactl --localalloc --cpunodebind"
+fi
+
+# Parameter Validation
+JAVA="$JAVA_HOME/bin/java"
+JAVA_FAMILY=`$JAVA -version 2>&1 >/dev/null | sed -n -e 's/.*version "1\.8\..*/JDK_8/p' -e 's/.*version "[1-9][0-9]\..*/JDK_X/p'`
+if [ "$JAVA_FAMILY" == "" ]
+then
+    echo "ERROR: Could not determine version of 'java' executable. Exiting."
+    exit 1
+fi
+
+if [ ! -e "$JBB_HOME/config" ]
+then
+    echo "ERROR: Can't stat '$JBB_HOME/config' check JBB_HOME."
+    exit 1
+fi
+
+# Set system parameters
+if [ $SET_SYSTEM = "Yes" ]
+then
+        nn=`id -u`
+        if [ ${nn} != "0" ]
+        then
+            echo "ERROR: Should be run as root."
+            exit 1
+        fi
+
+        for opt in $SYSCTL_OPTS
+        do
+            sysctl -w $opt
+        done
+
+        for opt in $SYSFILE_OPTS
+        do
+            filename=`echo $opt | sed -n -e 's/=.*$//p'`
+            value=`echo $opt | sed -n -e 's/.*=//p'`
+            echo $value | tee $filename
+        done
+fi
+
+# ----- Reporting ----------------
 echo "VERSION: $OPT_VERSION" > options.txt
+echo "JBB_HOME: $JBB_HOME" >> options.txt
 echo "JAVA_HOME: $JAVA_HOME" >> options.txt
+echo "JAVA_FAMILY: $JAVA_FAMILY" >> options.txt
 echo "GROUP_COUNT: $GROUP_COUNT" >> options.txt
-echo "PAGE_SIZE: $PAGE_SIZE" >> options.txt
-echo "PAGES: $PAGES" >> options.txt
-echo "YOUNG PAGES: $YOUNG_PAGES" >> options.txt
+echo "TI_JVM_COUNT: $TI_JVM_COUNT" >> options.txt
 echo "NUMA: $NUMA" >> options.txt
-echo "YOUNG: $mem_young" >> options.txt
-echo "NUM_OF_RUNS: $NUM_OF_RUNS" >> options.txt
-echo "$SPEC_OPTS_C"  | sed -e 's/ \+/\nSPEC_OPTS_Cx: /g' >> options.txt 
-echo "$JAVA_OPTS_C"  | sed -e 's/ \+/\nJAVA_OPTS_Cx: /g' >> options.txt 
-echo "$JAVA_OPTS_TI" | sed -e 's/ \+/\nJAVA_OPTS_TI: /g' >> options.txt 
-echo "$JAVA_OPTS_BE" | sed -e 's/ \+/\nJAVA_OPTS_BE: /g' >> options.txt 
+echo "$SPEC_OPTS"  | sed -e 's/ \+/\nSPEC_OPTS: /g' >> options.txt
+echo "$JAVA_OPTS"  | sed -e 's/ \+/\nJAVA_OPTS: /g' >> options.txt
+echo "DEBUG_OPTS: $DEBUG_OPTS" >> options.txt
+echo "$SYSCTL_OPTS" | sed -e 's/ \+/\nSYSCTL_OPTS: /g' >> options.txt
+echo "$SYSFILE_OPTS" | sed -e 's/ \+/\nSYSFILE_OPTS: /g' >> options.txt
+echo "USING JAVA:"
+$JAVA_HOME/bin/java -version >> options.txt 2>&1
 
 # END of OPTIONS
--- specjbb/runme.sh
+++ specjbb/runme.sh
@@ -1,151 +1,79 @@
 #!/bin/bash
+# Script for running SPECjbb2015, supports JDK11 or later
 
-VERSION="2.07 2020-10-30"
+VERSION="3.01 2025-08-23"
 
-###############################################################################
-# Sample script for running SPECjbb2015 in MultiJVM mode.
-# 
-# This sample script demonstrates running the Controller, TxInjector(s) and 
-# Backend(s) in separate JVMs on the same server.
-###############################################################################
-
-# Number of successive runs
-if [ "x$1" == "x" ]
-then	
-    NUM_OF_RUNS=1
-else
-    NUM_OF_RUNS=$1
-fi
+NUM_OF_RUNS=1
+[ "x$1" != "x" ] && NUM_OF_RUNS=$1
 
 script_path=$(cd "$(dirname "$0")"; pwd)
 
 # For options look at options.sh
 . $script_path/options.sh
 
-# Functions
-
-# Run TX + BE, with numa
-# Parameters GroupID, SocketID
-
+# Run TX + BE, optionally with numa
+# Parameters: GroupID, SocketID
 function run_group() {
     gnum=$1
     cpunode=$2
 
+    [ "x$NUMA" = "xYes" ] && THE_NUMA_CMD="${NUMA_CMD}=$cpunode"
+
     GROUPID=Group$gnum
     echo -e "\nStarting JVMs from $GROUPID:"
 
+    # Injector
     JVMID=tiJVM$gnum
     TI_NAME=$GROUPID.TxInjector.$JVMID
-    if [ $JAVA_FAMILY = "JDK_8" ]
-    then
-      DEBUG_OPTS_TI="-XX:+PrintGC -XX:+PrintGCDetails -Xloggc:${TI_NAME}.gc.log"
-    else
-      DEBUG_OPTS_TI="-Xlog:gc=debug,heap*=debug,phases*=debug,gc+age=debug:${TI_NAME}.gc.log"
-    fi
+    DEBUG_OPTS_TI="${DEBUG_OPTS}:${TI_NAME}.gc.log"
 
     echo "    Start $TI_NAME"
-    CMD_TI="$JAVA $JAVA_OPTS_TI $DEBUG_OPTS_TI $SPEC_OPTS_TI -jar ${JBB_HOME}/specjbb2015.jar -m TXINJECTOR -G=$GROUPID -J=$JVMID $MODE_ARGS_TI" 
-    echo $CMD_TI > ${TI_NAME}.cmdline.txt
+    CMD_TI="$JAVA $JAVA_OPTS_TI $DEBUG_OPTS_TI $SPEC_OPTS_TI -jar ${JBB_HOME}/specjbb2015.jar -m TXINJECTOR -G=$GROUPID -J=$JVMID"
+    echo $THE_NUMA_CMD $CMD_TI > ${TI_NAME}.cmdline.txt
+    $THE_NUMA_CMD $CMD_TI > $TI_NAME.log 2>&1 &
 
-    if [ "x$NUMA" = "xYes" ]
-    then
-        numactl --cpunodebind=$cpunode --localalloc $CMD_TI > $TI_NAME.log 2>&1 &
-    else  
-        $CMD_TI > $TI_NAME.log 2>&1 &
-    fi  
     echo -e "\t$TI_NAME PID = $!"
 
-    sleep 5 
-
+    # Backend
     JVMID=beJVM$gnum
     BE_NAME=$GROUPID.Backend.$JVMID
-    if [ $JAVA_FAMILY = "JDK_8" ]
-    then
-      DEBUG_OPTS_BE="-XX:+PrintGC -XX:+PrintGCDetails -Xloggc:${BE_NAME}.gc.log"
-    else
-      DEBUG_OPTS_BE="-Xlog:gc=debug,heap*=debug,phases*=debug,gc+age=debug:${BE_NAME}.gc.log"
-    fi
+    DEBUG_OPTS_BE="${DEBUG_OPTS}:${BE_NAME}.gc.log"
 
     echo "    Start $BE_NAME"
-    CMD_BE="$JAVA $JAVA_OPTS_BE $DEBUG_OPTS_BE $SPEC_OPTS_BE -jar ${JBB_HOME}/specjbb2015.jar -m BACKEND -G=$GROUPID -J=$JVMID $MODE_ARGS_BE"
-    echo $CMD_BE > ${BE_NAME}.cmdline.txt
-
-    if [ "x$NUMA" = "xYes" ]
-    then
-        numactl --cpunodebind=$cpunode --localalloc $CMD_BE > $BE_NAME.log 2>&1 &
-    else
-        $CMD_BE > $BE_NAME.log 2>&1 &
-    fi  
+    CMD_BE="$JAVA $JAVA_OPTS_BE $DEBUG_OPTS_BE $SPEC_OPTS_BE -jar ${JBB_HOME}/specjbb2015.jar -m BACKEND -G=$GROUPID -J=$JVMID"
+    echo $THE_NUMA_CMD $CMD_BE > ${BE_NAME}.cmdline.txt
+    $THE_NUMA_CMD $CMD_BE > $BE_NAME.log 2>&1 &
 
     echo -e "\t$BE_NAME PID = $!"
-    sleep 5 
 }
 
-###############################################################################
-# This benchmark requires a 1.8.x or 11 and later version of Java 
-# Check options.sh file for benchmark parameters and set_system.sh for OS tuning
-###############################################################################
-
-echo "RunMe $VERSION Numa: $NUMA Pages: $PAGES ($mem_max/$mem_young) GROUPS: $GROUP_COUNT"
-
-JAVA="$JAVA_HOME/bin/java"
+echo "RunMe $VERSION Numa: $NUMA GROUPS: $GROUP_COUNT ($TI_VMS_COUNT)"
 echo "Using JDK: $JAVA_HOME"
-$JAVA -version 2>&1 >/dev/null
-
-JAVA_FAMILY=`$JAVA -version 2>&1 >/dev/null | sed -n -e 's/.*version "1\.8\..*/JDK_8/p' -e 's/.*version "1[0-9]\..*/JDK_X/p'`
-if [ "$JAVA_FAMILY" == "" ]
-then
-    echo "ERROR: Could not determine version of 'java' executable. Exiting."
-    exit 1
-else
-    echo "JAVA Family is $JAVA_FAMILY"
-fi
-
-if [ ! -e "$JBB_HOME/config" ]
-then
-    echo "ERROR: Can't stat '$JBB_HOME/config' check JBB_HOME."
-    exit 1
-else    
-    echo "JBB: $JBB_HOME"
-fi
+echo "JAVA Family is $JAVA_FAMILY"
+echo "JBB: $JBB_HOME"
 
 if [ "$VALIDATE_ENV" == "Yes" ]
 then
-    nn=`id -u`
-    if [ ${nn} != "0" ]
-    then
-        echo "ERROR: Should be run as root."
-        exit 1
-    fi
-
-    if ps ax | grep -v grep | grep -q specjbb 
+    if ps ax | grep -v grep | grep -q specjbb
     then
         echo "ERROR: SPECjbb is already running."
         exit 1
-    fi	
+    fi
 
     if pgrep -c java >/dev/null
     then
         echo "Warning! java is already running. Results might suffer."
-    fi	
+    fi
 
     if pgrep -c docker >/dev/null
     then
         echo "Warning! DOCKER is running. Results might suffer."
-    fi	
+    fi
 fi
-
-if [ -e $script_path/set_system.sh ]
-then
-    echo -e "\nSet system configuration"
-    echo "FILE: $script_path/set_system.sh"
-    . $script_path/set_system.sh
-    echo ""
-fi  
 
 for ((n=1; $n<=$NUM_OF_RUNS; n=$n+1)); do
 
-    # Create result directory                
+    # Create result directory
     timestamp=$(date '+%y-%m-%d_%H%M%S')
     result=./$timestamp
     mkdir $result
@@ -160,31 +88,27 @@ for ((n=1; $n<=$NUM_OF_RUNS; n=$n+1)); do
     cp /proc/cpuinfo .
     cp /proc/version .
 
-    if [ -e $script_path/options.txt ] 
+    if [ -e $script_path/options.txt ]
     then
         cp $script_path/options.txt .
     fi
 
     cp $script_path/options.sh .
-    cp $script_path/set_system.sh .
     cp $script_path/README.md .
 
     echo "Run $n: $timestamp"
-
-    # Support 1, 2 and 4 groups on 2 sockets
-    # - 4 groups on 1 socket and 3 groups doesn't have sence
-    # - run composite for 0 groups
 
     if [ $GROUP_COUNT -ge 1 ]
     then
         echo "Launching SPECjbb2015 in MultiJVM mode..."
         echo
         echo "Start Controller JVM"
-        $JAVA $JAVA_OPTS_C $SPEC_OPTS_C -jar ${JBB_HOME}/specjbb2015.jar -m MULTICONTROLLER $MODE_ARGS_C 2>controller.log > controller.out &
+        $JAVA $JAVA_OPTS_CTL $SPEC_OPTS_CTL -jar ${JBB_HOME}/specjbb2015.jar -m MULTICONTROLLER 2>controller.log > controller.out &
         CTRL_PID=$!
         echo "Controller PID = $CTRL_PID"
         sleep 5
 
+        # Support 1, 2, 4 groups
         run_group 0 0
         if [ $GROUP_COUNT -ge 2 ]
         then
@@ -198,56 +122,31 @@ for ((n=1; $n<=$NUM_OF_RUNS; n=$n+1)); do
 
         echo
         echo "SPECjbb2015 is running..."
-        if [ "x${TAIL_WAIT}" = "xYes" ]
-        then
-            echo "Monitoring $result/controller.out for progress"
-            tail --pid=${CTRL_PID} -f ./controller.out
-        else
-            echo "Please monitor $result/controller.out for progress"
-            wait $CTRL_PID
-        fi
+        echo "Monitoring $result/controller.out for progress"
+        tail --pid=${CTRL_PID} -f ./controller.out
         echo
         echo "Controller has stopped"
 
     else
         echo "Launching SPECjbb2015 in CompositeJVM mode..."
-        if [ "x$NUMA" = "xYes" ]
-        then
-            echo "Warning! Composite will be bound to socket 0"
-        fi
-        echo
-        BE_NAME="Composite"
-        if [ $JAVA_FAMILY = "JDK_8" ]
-        then
-           DEBUG_OPTS_BE="-XX:+PrintGC -XX:+PrintGCDetails -Xloggc:${BE_NAME}.gc.log"
-        else
-           DEBUG_OPTS_BE="-Xlog:gc=debug,heap*=debug,phases*=debug,gc+age=debug:${BE_NAME}.gc.log"
-        fi
-        CMD_BE="$JAVA $JAVA_OPTS_BE $DEBUG_OPTS_BE $SPEC_OPTS_C $SPEC_OPTS_BE -jar ${JBB_HOME}/specjbb2015.jar -m COMPOSITE $MODE_ARGS_BE"
-        echo $CMD_BE > ${BE_NAME}.cmdline.txt
-        if [ "x$NUMA" = "xYes" ]
-        then
-            numactl --cpunodebind=0 --localalloc $CMD_BE > $BE_NAME.log 2>&1 &
-        else
-            $CMD_BE > $BE_NAME.log 2>&1 &
-        fi
-        BE_PID=$!
-        echo -e "\t$BE_NAME PID = $BE_PID"
+        [ "x$NUMA" = "xYes" ] && NUMA_CMD="${NUMA_CMD}=0"
+        NAME="Composite"
+        DEBUG_OPTS="${DEBUG_OPTS}:${NAME}.gc.log"
+        CMD="$JAVA $JAVA_OPTS $DEBUG_OPTS $SPEC_OPTS -jar ${JBB_HOME}/specjbb2015.jar -m COMPOSITE"
+        echo $NUMA_CMD $CMD > ${NAME}.cmdline.txt
+        $NUMA_CMD $CMD > $NAME.log 2>&1 &
+
+        PID=$!
+        echo -e "\t$NAME PID = $PID"
         echo
         echo "SPECjbb2015 is running..."
-        if [ "x${TAIL_WAIT}" = "xYes" ]
-        then
-            echo "Monitoring $result/${BE_NAME}.log for progress"
-            tail --pid=${BE_PID} -f ./${BE_NAME}.log
-        else
-            echo "Please monitor $result/${BE_NAME}.log for progress"
-            wait $BE_PID
-        fi
+        echo "Monitoring $result/${NAME}.log for progress"
+        tail --pid=${BE_PID} -f ./${NAME}.log
     fi
 
     echo "SPECjbb2015 has finished"
     echo
-  
+
     cd ..
 
 done
